7a0ea7d5d7d9f762a89f67bb8077cb5ef38726f1
Ke Tang <jiushizhutk@gmail.com>
2020-06-04 22:15:56 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # slack-post-action
-GitHub Action for Slack Posting
+Simple GitHub Action To Send Slack Message
+
+## Inputs
+
+### `SLACK_WEBHOOK`
+
+**Required** The slack webhook url. Default `""`.
+
+### `MESSAGE`
+
+**Required** The message to send. Default `""`.
+
+## Example usage
+
+uses: actions/slack-post-action@v1
+with:
+  SLACK_WEBHOOK: ${{ secrets.SLACK_WEBHOOK }}
+  MESSAGE: "Success"
---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const request = require('request');
+
+try {
+  const slackWebhookUrl = core.getInput('SLACK_WEBHOOK');
+  const message = core.getInput('MESSAGE');
+
+  if (!slackWebhookUrl || !message) {
+      console.log('Invalid input');
+  } else {
+    const options = {
+        url: slackWebhookUrl,
+        json: true,
+        body: {
+            text: message
+        }
+    };
+    
+    request.post(options, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Status: ${res.statusCode}`);
+        console.log(body);
+    });
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
